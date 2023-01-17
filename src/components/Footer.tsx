1a9404cad7677aec398cb74adc4d013c11311af4
Rhamon Guedes <rhamon.guedes@al.infnet.edu.br>
2023-01-17 14:28:35 -0300

    header, footer and read.me

---
+++ src/components/Footer.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from './Footer.module.css'
+
+const Footer = () => {
+  return (
+    <div>
+      <footer className={styles.footer}>
+        <p>
+          <span>React + TS Todo</span> @ 2021
+        </p>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
